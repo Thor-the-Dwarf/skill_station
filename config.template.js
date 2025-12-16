66233469f21e257ebb63a178ab38d12a5f92a0a1
Thor <thor.schott@gmail.com>
2025-12-16 10:00:16 +0100

    API-Key changed and restricted + index hopefully fixed

--- config.template.js
+++
@@ -1,9 +0,0 @@
-/* config.template.js
- * 
- * Vorlage für die globale Konfiguration.
- * Kopiere diese Datei zu 'config.js' und trage dort deine eigenen API-Keys ein.
- */
-window.AppConfig = {
-    apiKey: 'AIzaSyDut0YzAuIBTWov1q_Wr3edruzKIAB-PAY',
-    // clientId: 'YOUR_CLIENT_ID_HERE', 
-};
